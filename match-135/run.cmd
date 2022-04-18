35668b90ad9387bd05e92ae874a78090d72d6e8e
Naum Azeredo <naumazeredo@gmail.com>
2022-04-17 23:13:59 -0700

    Update run cmd again

--- match-135/run.cmd
+++ match-135/run.cmd
@@ -9,20 +9,23 @@ if "%CC%" == "" (
 
   rmdir /s /q build
   mkdir build
+  pushd build
 
   echo Building...
   echo.
 
   if "%CC%" == "cl" (
-    %CC% %OPTS% /std:c++17 /EHsc /Fe"./build/solution" ./src/solution/*.cpp
-    %CC% %OPTS% /std:c++17 /EHsc /Fe"./build/stats" ./src/tools/stats.cpp
+    %CC% %OPTS% /std:c++17 /EHsc /I../src /Fe"./solution" ../src/solution/*.cpp
+    %CC% %OPTS% /std:c++17 /EHsc /I../src /Fe"./stats" ../src/tools/stats.cpp
   ) else if "%CC%" == "g++" (
-    %CC% %OPTS% -std=c++17 -o "./build/solution" ./src/solution/*.cpp
-    %CC% %OPTS% -std=c++17 -o"./build/stats" ./src/tools/stats.cpp
+    %CC% %OPTS% -std=c++17 -I../src -o "./solution" ../src/solution/*.cpp
+    %CC% %OPTS% -std=c++17 -I../src -o"./stats" ../src/tools/stats.cpp
   ) else (
     echo CC not cl or gcc
     exit 1
   )
+
+  popd
 
   echo.
   echo Build done!
---
+++ match-135/src/tools/stats.cpp
@@ -0,0 +1,5 @@
+#include "common/state.h"
+
+int main() {
+  return 0;
+}
