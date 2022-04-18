1e568df640824c214f45003f77f4338d9b904a53
Naum Azeredo <naumazeredo@gmail.com>
2022-04-17 23:08:45 -0700

    Update run cmd

--- match-135/run.cmd
+++ match-135/run.cmd
@@ -1,24 +1,33 @@
 @echo off
 
 @REM check env vars
-if "%CC%" equ "" (
+if "%CC%" == "" (
   echo CC not set. Use run-cl.cmd or run-gcc.cmd or set CC
   exit 1
+) else (
+  @REM build
+
+  rmdir /s /q build
+  mkdir build
+
+  echo Building...
+  echo.
+
+  if "%CC%" == "cl" (
+    %CC% %OPTS% /std:c++17 /EHsc /Fe"./build/solution" ./src/solution/*.cpp
+    %CC% %OPTS% /std:c++17 /EHsc /Fe"./build/stats" ./src/tools/stats.cpp
+  ) else if "%CC%" == "g++" (
+    %CC% %OPTS% -std=c++17 -o "./build/solution" ./src/solution/*.cpp
+    %CC% %OPTS% -std=c++17 -o"./build/stats" ./src/tools/stats.cpp
+  ) else (
+    echo CC not cl or gcc
+    exit 1
+  )
+
+  echo.
+  echo Build done!
 )
 
-@REM build
-
-echo Building...
-echo.
-
-rmdir /s /q build
-mkdir build
-pushd build
-%CC% /EHsc /Fe"solution" ../src/solution/main.cpp
-popd
-
-echo.
-echo Build done!
 
 @REM run
 echo Running...
